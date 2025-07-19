2e56d66cc04c7eab52731bdefd529ca5069a02a4
prannav-bansal <collabwithpranav@gmail.com>
2025-07-19 23:56:45 +0530

    env and server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const app = require('./app');
+const { config } = require('dotenv');
+config();
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
